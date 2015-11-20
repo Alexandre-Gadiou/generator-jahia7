216e8880581a7996d972b73caa7925b1b3ed4f00
Alexandre Gadiou <alexandregadiou44@icloud.com>
2015-11-20 01:59:26 +0100

    scripts

--- generators/app/templates/app/assets/javascript/script2.js
+++ generators/app/templates/app/assets/javascript/script2.js
@@ -1,22 +1,22 @@
 // Loose augmentation JS module pattern
 var app = (function (app, undefined) {
     'use strict';
-
+    
     app = app || {};
-
+    
     // Deal with possible jQuery conflicts
     var $ = app.$ || $;
-
+    
     /*
      * Private function example
      */
     var myFunction = function () {
-        console.log("page1");
-    };
-
+        console.log("script2");
+    };  
+    
     // Expose API
     app.myFunction = myFunction;
-
+            
     $(function () {
         // Execute function when DOM is ready
         myFunction();
