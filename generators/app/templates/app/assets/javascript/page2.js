de94700f9a44d3ec52eef4e9162f9e12c919d176
Alexandre Gadiou <alexandregadiou44@icloud.com>
2015-11-20 01:33:21 +0100

    20 november  update

---
+++ generators/app/templates/app/assets/javascript/page2.js
@@ -0,0 +1,26 @@
+// Loose augmentation JS module pattern
+var app = (function (app, undefined) {
+    'use strict';
+
+    app = app || {};
+
+    // Deal with possible jQuery conflicts
+    var $ = app.$ || $;
+
+    /*
+     * Private function example
+     */
+    var myFunction = function () {
+        console.log("page1");
+    };
+
+    // Expose API
+    app.myFunction = myFunction;
+
+    $(function () {
+        // Execute function when DOM is ready
+        myFunction();
+    });
+
+    return app;
+}(app));
